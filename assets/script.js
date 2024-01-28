cc31980aaad1ed5269c4a1869b04b27d570ed3f9
Yaseen fehad <yaseenfehad@gmail.com>
2024-01-28 23:35:11 +0000

    added ids to html and used jquery to structure the dynamically generated html

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,34 @@
 console.log("hello world");
-var city;  
-var weather = $(this).attr("city-forcast");
+
 var apiKey = "94a9dd50d9451c24e2dc7b65763db4db";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-$("button").on("click", function () {
-    function fetchWeather(city) {
-        fetch(queryURL)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data);
+// Function to fetch weather data
+function fetchWeather(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-                var results = data.data;
-                displayweather(city, data); 
-            });
-    }
-
-    fetchWeather(city);
-});
-
-function displayweather(city) {
-    const name = city;
-    const icon = data.weather[0].icon;  
-    const temp = data.main.temp;
-    const humidity = data.main.humidity;
-    const windSpeed = data.wind.speed; 
+    return fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .catch(function (error) {
+            console.error('Error during fetch:', error);
+        });
 }
+
+
+function displayWeather(data) {
+    const name = data.name;
+    const icon = data.weather[0].icon;
+    const temp = Math.round(data.main.temp -273);
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
+
+    $("#city").text("City: " + name);
+    $("#weather-info").html(`
+        <p>Weather: ${data.weather[0].main}</p>
+        <p>Temperature: ${temp} °C</p>
+        <p>Humidity: ${humidity}%</p>
+        <p>Wind Speed: ${windSpeed} m/s</p>
+    `);
+}
+
