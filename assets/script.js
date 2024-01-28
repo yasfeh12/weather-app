5be1d6c32708ba9da1c5a9f0e1ab79ea9406aa07
Yaseen fehad <yaseenfehad@gmail.com>
2024-01-28 15:09:34 +0000

    started script.js by ensuing its linked with html then set basic variables for api began the functions for fetching and displaying the weather

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,30 @@
 console.log("hello world");
+var city;  
+var weather = $(this).attr("city-forcast");
+var apiKey = "94a9dd50d9451c24e2dc7b65763db4db";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
 $("button").on("click", function () {
-    var city;
-    var weather = $(this).attr("city-forcast");
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid"+ apiKey;
-    var apiKey= "&api_key=94a9dd50d9451c24e2dc7b65763db4db";
-    fetch(queryURL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-      
-        console.log(data);
-        console.log(data.data);
-  
-        var results = data.data;
-   
-      });
-  });
+    function fetchWeather(city) {
+        fetch(queryURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+
+                var results = data.data;
+                displayweather(city, data); 
+            });
+    }
+
+    fetchWeather(city);
+});
+
+function displayweather(city) {
+    const name = city;
+    const icon = data.weather[0].icon;  
+    const temp = data.main.temp;
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed; 
+}
