e21e1413258802b8aaa1dc4e0abe59a991994099
Yaseen fehad <yaseenfehad@gmail.com>
2024-01-30 15:35:30 +0000

    changed the api key so it collects data for 5 days added a for loop to add populate the data in 5 different cards for each day.

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ console.log("hello world");
 var apiKey = "94a9dd50d9451c24e2dc7b65763db4db";
 
 function fetchWeather(city) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
     return fetch(queryURL)
         .then(function (response) {
@@ -14,25 +14,36 @@ function fetchWeather(city) {
         });
 }
 
-
 function displayWeather(data) {
-    console.log(data.weather[0].icon)
-    const name = data.name;
-    const icon = data.weather[0].icon;
-    const temp = Math.round(data.main.temp -273);
-    const humidity = data.main.humidity;
-    const windSpeed = data.wind.speed;
-    $("#weather-card").css("display", "block");
-    $(".icon").attr("src", "https://openweathermap.org/img/wn/" + icon + "@2x.png");
+    if (data.list && data.list.length >= 5) {
+        for (let i = 0; i < 5; i++) {
+            const name = data.city.name;
+            const icon = data.list[i].weather[0].icon;
+            const temp = Math.round(data.list[i].main.temp - 273);
+            const humidity = data.list[i].main.humidity;
+            const windSpeed = data.list[i].wind.speed;
 
-    $("#city").text("City: " + name);
-    $("#weather-info").html(`
-        <p>Weather: ${data.weather[0].main}</p>
-        <p>Temperature: ${temp} °C</p>
-        <p>Humidity: ${humidity}%</p>
-        <p>Wind Speed: ${windSpeed} m/s</p>
-    `);
+   
+            const $weatherCard = $(".weather-card").eq(i);
+            
+    
+            $weatherCard.find(".city").text("City: " + name);
+            $weatherCard.find(".icon").attr("src", "https://openweathermap.org/img/wn/" + icon + "@2x.png");
+            $weatherCard.find(".weather-info").html(`
+                <p>Weather: ${data.list[i].weather[0].main}</p>
+                <p>Temperature: ${temp} °C</p>
+                <p>Humidity: ${humidity}%</p>
+                <p>Wind Speed: ${windSpeed} m/s</p>
+            `);
+
+         
+            $weatherCard.css("display", "block");
+        }
+    } else {
+        console.error("Invalid data format or insufficient forecast items.");
+    }
 }
+
 $("#search-card").submit(function (event) { 
     event.preventDefault();
     var city = $("#cityInput").val().trim();
@@ -47,6 +58,8 @@ $("#search-card").submit(function (event) {
             });
     }
 });
+
+
 
 $("#new-city-btn").click(function () {
 
